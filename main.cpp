5e019e69c52c76c205cc4e36ef05bc99a6c30e4b
Resham <resha@MacBook-Air-2.local>
2025-11-02 18:56:25 -0800

    Added driver in the main

--- main.cpp
+++ main.cpp
@@ -14,6 +14,44 @@ int main() {
     villagerDetails["Kyle"] = {10, "Wolf", "Hubba hubba!"};
     villagerDetails.insert({"Raymond", {8, "Cat", "Nice fit"}});
 
+    int choice;
+    string villagerName;
+    bool isExit = false;
+
+    cout << "1. Increase Friendship" << endl;
+    cout << "2. Decrease Friendship" << endl;
+    cout << "3. Search for Villager" << endl;
+    cout << "4. Exit" << endl;
+
+    cin >> choice;
+
+    while (isExit == false)
+    {
+        switch (choice)
+        {
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                cout << "Please enter villager's name: ";
+                cin >> villagerName;;
+                auto it = villagerDetails.find(villagerName);
+                if (it != villagerDetails.end()) {  // the iterator points to beyond the end of the map
+                                                // if searchKey is not found
+                    cout << "\nFound " << villagerName << "'s details: ";
+                    auto [friendship, species, catchphrase] = it->second;
+                    cout << " [" << friendship << ", " << species << ", " << catchphrase << "]";
+                    cout << endl;
+                } else
+                    cout << endl << villagerName << " not found." << endl;
+                break;
+            case 4:
+                isExit = true;
+                break;
+        }
+    }
+
     // access the map using a range-based for loop
     cout << "Villagers and their details (range-based for loop):" << endl;
     for (const auto& [name, details] : villagerDetails) 
@@ -22,33 +60,6 @@ int main() {
         cout << name << " [" << friendship << ", " << species << ", " << catchphrase << "]";
         cout << endl;
     }
-
-    // access the map using iterators
-    cout << "\nVillagers and their details (iterators):" << endl;
-    for (map<string, tuple<int, string, string>>::iterator it = villagerDetails.begin(); 
-                                               it != villagerDetails.end(); ++it) 
-    {
-        const string& name = it->first;
-        auto [friendship, species, catchphrase] = it->second;
-
-        cout << name << " [" << friendship << ", " << species << ", " << catchphrase << "]";
-        cout << endl;
-    }
-
-    // delete an element
-    villagerDetails.erase("Raymond");
-
-    // search for an element using .find() to avoid errors
-    string searchKey = "Kyle";
-    auto it = villagerDetails.find(searchKey);
-    if (it != villagerDetails.end()) {  // the iterator points to beyond the end of the map
-                                       // if searchKey is not found
-        cout << "\nFound " << searchKey << "'s details: ";
-        auto [friendship, species, catchphrase] = it->second;
-        cout << " [" << friendship << ", " << species << ", " << catchphrase << "]";
-        cout << endl;
-    } else
-        cout << endl << searchKey << " not found." << endl;
 
     // report size, clear, report size again to confirm map operations
     cout << "\nSize before clear: " << villagerDetails.size() << endl;
